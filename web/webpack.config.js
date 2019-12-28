d9f492652d7f6d69cbb2fb9f59bb23dbd65a4f2d
JongHwa <31912670+jonghwajoa@users.noreply.github.com>
2019-12-28 14:34:12 +0900

    chore: webpack 플러그인 추가

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -10,7 +11,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js?$/,
+        test: /\.(js?|ts?)$/,
         loader: 'babel-loader',
         exclude: /node_modules/,
       },
@@ -22,5 +23,27 @@ module.exports = {
     filename: 'bundle.js',
   },
 
-  plugins: [new HtmlWebpackPlugin({ template: `./public/index.html` })],
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    compress: false,
+    port: 3000,
+    stats: {
+      colors: true,
+      hash: false,
+      version: false,
+      timings: false,
+      assets: false,
+      chunks: false,
+      modules: false,
+      reasons: false,
+      children: false,
+      source: false,
+      errors: true,
+      errorDetails: true,
+      warnings: false,
+      publicPath: false,
+    },
+  },
+
+  plugins: [new CleanWebpackPlugin(), new HtmlWebpackPlugin({ template: './public/index.html' })],
 };
