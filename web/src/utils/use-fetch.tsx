87b15741a44051b0a1456b0c951d602c27b767cd
Seong <pok_gare@naver.com>
2020-01-06 18:53:25 +0900

    fix(fe): use fetch 수정
    TS로 옮겨옴에 따라 엄격한 타입 체크를 위해
    setState시 들어가는 인자 수정
    또 이렇게 하는게 맞는거 같아 보임
    error에는 boolean, data에는 data만 들어가게

--- web/src/utils/use-fetch.tsx
+++ web/src/utils/use-fetch.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import request from './request';
+import HTTPResponse from './http-response';
 
 interface IfetchData {
   isLoading: boolean;
@@ -9,23 +10,31 @@ interface IfetchData {
 
 const initialValues: IfetchData = { isLoading: true, error: false, data: null };
 
-const useFetch = (URL: string) => {
+const useFetch = (url: string) => {
   const [fetchingData, setFetchingData] = useState<IfetchData>(initialValues);
 
   useEffect(() => {
     const fetchInitData = async () => {
       setFetchingData(initialValues);
 
-      const { isError, data } = await request.get(URL);
-      if (isError) {
-        setFetchingData({ isLoading: false, error: data, data: null });
+      const response: HTTPResponse = await request.get({ url });
+      if (response.getIsError()) {
+        setFetchingData({
+          isLoading: false,
+          error: response.getIsError(),
+          data: response.getData(),
+        });
       } else {
-        setFetchingData({ isLoading: false, error: null, data });
+        setFetchingData({
+          isLoading: false,
+          error: response.getIsError(),
+          data: response.getData(),
+        });
       }
     };
 
     fetchInitData();
-  }, [URL]);
+  }, [url]);
 
   return fetchingData;
 };
