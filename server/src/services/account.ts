91a606d583744c654fad77b56606982bd876222d
JongHwa <31912670+jonghwajoa@users.noreply.github.com>
2020-01-12 20:31:11 +0900

    feat(server/login): 로그인 로직 구현 bcrpyt를 이용하여 compare를 한다.

--- server/src/services/account.ts
+++ server/src/services/account.ts
@@ -7,12 +7,22 @@ interface ILogin {
   pw: string;
 }
 
+// TODO : VALIDATION
 export const login = async ({ id, pw }: ILogin): Promise<Boolean> => {
-  console.log(`id는 ${id}`);
+  const userRepo = getRepository(User);
+  const user = await userRepo.findOne({ id });
+  if (!user) {
+    throw "존재하지 않는 유저입니다";
+  }
 
-  if (pw !== "가나다") {
-    return false;
+  const { pw: hashedPw } = user;
+  const isSamePassword: boolean = await bcrypt.compare(pw, hashedPw);
+  if (!isSamePassword) {
+    throw "비밀번호 일치안함.";
   }
 
   return true;
 };
+
+// const salt = await bcrypt.genSalt(10);
+// const hashed = await bcrypt.hash(pw, salt);
