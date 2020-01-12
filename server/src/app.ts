24664fdcae77bf56098b19cba5f9da5e2cdd91c4
JongHwa <31912670+jonghwajoa@users.noreply.github.com>
2020-01-12 14:46:28 +0900

    fix(server): 라우터 제거 graphql을 사용할것이기 때문에 엔드포인트가 필요없음

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,21 +2,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import Koa from "koa";
-import Router from "koa-router";
 import "reflect-metadata";
 import { createConnection } from "typeorm";
 
 import server from "./graphql";
-import api from "./api";
 
 const { PORT } = process.env;
 const app = new Koa();
-const router = new Router();
 
 server.applyMiddleware({ app });
-
-router.use(api.routes());
-app.use(router.routes());
 
 createConnection().then(() => {
   console.log("db connection");
