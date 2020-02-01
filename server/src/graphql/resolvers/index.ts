9f438d32cd59ff76875c2f480c7de343ce5b1db8
Seong <pok_gare@naver.com>
2020-02-02 01:17:59 +0900

    fix(be): resolvers를 네이밍 컨벤션을 따르도록 수정

--- server/src/graphql/resolvers/index.ts
+++ server/src/graphql/resolvers/index.ts
@@ -1,7 +1,7 @@
 import { fileLoader, mergeResolvers } from "merge-graphql-schemas";
 import path from "path";
 
-const resolverFiles = fileLoader(path.resolve(__dirname, "."));
+const resolverFiles = fileLoader(path.resolve(__dirname, "./**/*.resolvers.*"));
 const resolvers = mergeResolvers(resolverFiles);
 
 export { resolvers };
