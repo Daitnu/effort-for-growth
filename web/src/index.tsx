b3d9e67b5c97e7c98d923c5e265c2fd34c021d00
Seong <pok_gare@naver.com>
2020-02-12 20:46:25 +0900

    feat(fe): graphql client 설정
    apollo provider 추가
    package 추가

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
 import AppRouter from './router';
 
-ReactDOM.render(<AppRouter />, document.getElementById('root'));
+const client = new ApolloClient({
+  uri: 'http://localhost/graphql',
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <AppRouter />
+  </ApolloProvider>,
+  document.getElementById('root'),
+);
