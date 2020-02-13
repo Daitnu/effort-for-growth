d27bfde154b65b67d17ad60edcf35199dd263d4f
JongHwa <31912670+jonghwajoa@users.noreply.github.com>
2020-02-13 23:31:53 +0900

    fix(fe/error): 통일성을 위해 FC로 변경

--- web/src/pages/error/Error.tsx
+++ web/src/pages/error/Error.tsx
@@ -4,4 +4,4 @@ interface Props {
   message: string;
 }
 
-export const ErrorPage: React.FunctionComponent<Props> = ({ message }) => <div>{message}</div>;
+export const ErrorPage: React.FC<Props> = ({ message }) => <div>{message}</div>;
