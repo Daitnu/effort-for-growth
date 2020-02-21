81e35ff1caab9503e3806822010cb62553858203
Seong <pok_gare@naver.com>
2020-02-21 16:27:44 +0900

    feat(fe): 회원가입 에러메세지 state 추가

--- web/src/components/GlobalStyle/Forms/Register/RegisterForm.tsx
+++ web/src/components/GlobalStyle/Forms/Register/RegisterForm.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './styled';
 import * as LS from '../Login/styled';
 import * as GS from '../../../GlobalStyle';
 import { useMutation } from '@apollo/react-hooks';
 import { gql, DocumentNode } from 'apollo-boost';
 
+interface IError {
+  id: string;
+  name: string;
+  pw: string;
+  pwCheck: string;
+}
+
+const initError: IError = {
+  id: '',
+  name: '',
+  pw: '',
+  pwCheck: '',
+};
+
 export const RegisterForm: React.FC = () => {
+  const [errorMsg, setErrorMsg] = useState<IError>(initError);
+
   return (
     <div>
       <LS.FormItemWithIcon>
@@ -14,14 +30,14 @@ export const RegisterForm: React.FC = () => {
         </LS.IconWrapper>
         <LS.FormInput type="text" id="id" placeholder="아이디" maxLength={20} autoComplete="off" />
       </LS.FormItemWithIcon>
-      아이디 에러
+      {errorMsg.id}
       <LS.FormItemWithIcon>
         <LS.IconWrapper>
           <S.UserNameIcon />
         </LS.IconWrapper>
         <LS.FormInput type="text" id="name" placeholder="이름" maxLength={20} autoComplete="off" />
       </LS.FormItemWithIcon>
-      이름 에러
+      {errorMsg.name}
       <LS.FormItemWithIcon>
         <LS.IconWrapper>
           <LS.PasswordIcon />
@@ -34,7 +50,7 @@ export const RegisterForm: React.FC = () => {
           maxLength={20}
         />
       </LS.FormItemWithIcon>
-      비밀번호 에러
+      {errorMsg.pw}
       <LS.FormItemWithIcon>
         <LS.IconWrapper>
           <LS.PasswordIcon />
@@ -47,7 +63,7 @@ export const RegisterForm: React.FC = () => {
           maxLength={20}
         />
       </LS.FormItemWithIcon>
-      비밀번호 확인 에러
+      {errorMsg.pwCheck}
       <S.RegisterButton>회원가입</S.RegisterButton>
     </div>
   );
