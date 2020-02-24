29812133aeab40d8df91a0e6fbdb66a45c9586c6
Seong <pok_gare@naver.com>
2020-02-25 04:29:34 +0900

    feat(fe): error fields class 추가
    프론트에서도 error fields를 쓰는것도 좋을 것 같음

---
+++ web/src/utils/error/error-field.ts
@@ -0,0 +1,11 @@
+export class ErrorField {
+  private readonly field: string;
+  private readonly value: string;
+  private readonly reason: string;
+
+  constructor(field: string, value: string = '', reason: string) {
+    this.field = field;
+    this.value = field.substr(0, 2) === 'pw' ? 'secret' : value;
+    this.reason = reason;
+  }
+}
