c838b1b80b3f84badfc6628539f783c0d666e657
Seong <pok_gare@naver.com>
2020-02-25 03:53:38 +0900

    feat(be): 에러 상수 추가
    회원가입시 중복된 아이디에 대한 에러 상수 추가

--- server/src/libraries/exception/constant.ts
+++ server/src/libraries/exception/constant.ts
@@ -4,6 +4,7 @@ export const ERROR = {
   INVALID_NAME_LENGTH: "name의 길이가 부적합 합니다.",
   INVALID_INPUT_VALUE: "INVALID INPUT VALUE",
   ACCOUNT: {
-    INVALID_ID_OR_PW: "가입하지 않은 아이디이거나, 잘못된 비밀번호입니다."
+    INVALID_ID_OR_PW: "가입하지 않은 아이디이거나, 잘못된 비밀번호입니다.",
+    DUPLICATED: "중복된 아이디입니다."
   }
 };
