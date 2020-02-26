8ec9d6dc575d58b8b58a3baf52f5d79d9cde76c9
Seong <pok_gare@naver.com>
2020-02-26 17:01:01 +0900

    feat(fe): 비밀번호와 일치하지 않다라는 메세지 상수 추가

--- web/src/utils/error/constant.ts
+++ web/src/utils/error/constant.ts
@@ -3,9 +3,13 @@ import { REGISTER_FIELDS } from '../../@types/validator-field-names';
 export const ERROR = {
   REGISTER: {
     LENGTH: {
-      [REGISTER_FIELDS.ID]: 'id의 길이가 부적합 합니다.',
-      [REGISTER_FIELDS.PW]: 'pw의 길이가 부적합 합니다.',
-      [REGISTER_FIELDS.NAME]: 'name의 길이가 부적합 합니다.',
+      [REGISTER_FIELDS.ID]: '아이디의 길이가 부적합 합니다.',
+      [REGISTER_FIELDS.PW]: '비밀번호의 길이가 부적합 합니다.',
+      [REGISTER_FIELDS.NAME]: '이름의 길이가 부적합 합니다.',
+      [REGISTER_FIELDS.PW_CONFIRM]: '비밀번호의 길이가 부적합 합니다.',
+    },
+    EQUAL: {
+      [REGISTER_FIELDS.PW_CONFIRM]: '비밀번호와 일치하지 않습니다.',
     },
   },
 };
