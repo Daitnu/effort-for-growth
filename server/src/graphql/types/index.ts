c049436e6e3df19b3763df9c9ca506a633c1b2de
JongHwa <31912670+jonghwajoa@users.noreply.github.com>
2020-02-27 22:49:39 +0900

    fix(be/graphql-types): graphql 파일만  load grahpql파일만 필요함으로 graphql 확장자 명시

--- server/src/graphql/types/index.ts
+++ server/src/graphql/types/index.ts
@@ -1,7 +1,9 @@
 import { fileLoader, mergeTypes } from "merge-graphql-schemas";
 import path from "path";
 
-const typeDefFiles: any[] = fileLoader(path.resolve(__dirname, "."));
+const typeDefFiles: any[] = fileLoader(
+  path.resolve(__dirname, "./**/*.graphql")
+);
 const typeDefs: string = mergeTypes(typeDefFiles);
 
 export { typeDefs };
